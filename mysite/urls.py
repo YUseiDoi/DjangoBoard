bdaa71da546513a0f9c8bc2d7bdcac8199f90df1
YUseiDoi <yusedo1101@gmail.com>
2021-04-09 23:30:59 +0900

    mac commit 20210409

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import base
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('thread/', include('thread.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
